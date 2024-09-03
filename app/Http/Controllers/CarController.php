9016ada6b6cf7b678cccdb1d16930ac4bb813108
AdrianaRashidi <adrianarashidi98@gmail.com>
2024-09-03 15:37:00 +0800

    bengkel

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Car;
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    public function index()
+    {
+        $cars = Car::paginate(5);
+
+        return view('cars.index', [
+            'cars' => $cars
+        ]);
+    }    
+
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cars', [App\Http\Controllers\CarController::class, 'index'])->name('cars.index');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
